60786a9343887c3ebad2e2415a581db0c16a2661
Josef Hrebec <josefhrebec02@gmail.com>
2022-12-09 18:14:19 +0100

    Add Emotion framework

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
+/** @jsxImportSource @emotion/react */
+
 import React from "react";
 import { Link } from "react-router-dom";
+import { css } from "@emotion/react";
+
+const Style = css`
+  padding: 32px;
+  background-color: hotpink;
+  font-size: large;
+  text-align: center;
+  color: white;
+  border-radius: 0 0 12px 12px;
+`;
 
 const Header = () => {
-  return null;
+  return <header css={Style}>Golden Glizzys</header>;
 };
 
 export { Header };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
+/** @jsxImportSource @emotion/react */
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Header } from "./components/header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Global, css } from "@emotion/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        * {
+          margin: 0;
+          padding: 0;
+        }
+      `}
+    />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
